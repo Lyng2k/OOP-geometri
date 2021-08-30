02adb261e24577b77f6db10d6ff71d7dec501467
zbcsjly <zbcsjly@ZBC-S-SIMO114Q>
2021-08-30 13:14:17 +0200

    Add project files.

---
+++ OOP geometri/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square(5);
+            
+            Console.WriteLine(square1.circumference());
+            Square square2 = new Square();
+            square2.Side = 4;
+            Console.WriteLine(square2.circumference());
+
+            Square square3 = new Square();
+            square3.Side = 5;
+            Console.WriteLine(square3.area());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP geometri/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_geometri
+{
+    public class Square
+    {
+        private int side;
+
+        public int Side
+        {
+            get
+            {
+                return side;
+            }
+            set
+            {
+                side = value;
+            }
+        }
+        public Square()
+        {
+
+        }
+        public Square(int side)
+        {
+            this.side = side;
+        }
+        public int area()
+        {
+            int resultat = side * side;
+            return resultat;
+        }
+        public int circumference()
+        {
+            int resultat = side * 4;
+            return resultat;
+        }
+
+    }
+}
